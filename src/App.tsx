197435d46b2ca3586b544f42cc8f8dc709ccf1ef
Ian Carlos Bertoncelo <icbertoncelo@MacBook-Pro-de-Ian.local>
2023-01-23 15:31:24 -0300

    Add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
 
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
@@ -6,9 +8,11 @@ import { defaultTheme } from './styles/themes/default'
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>SUZI</h1>
+      <BrowserRouter>
+        <Router />
 
-      <GlobalStyle />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
